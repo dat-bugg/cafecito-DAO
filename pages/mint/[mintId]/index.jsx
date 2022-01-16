8d927b2f4ca08dc1d2249cabb57bda9e9e525b1e
azaek <aloxel@gmail.com>
2022-01-16 18:30:00 +0530

    updated logo

--- pages/mint/[mintId]/index.jsx
+++ pages/mint/[mintId]/index.jsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 
 const svgPath = getSvgPath({
     width: 479,
-    height: 400,
+    height: 410,
     cornerRadius: 48,
     cornerSmoothing: 1
 });
 
 const svgPath2 = getSvgPath({
     width: 296,
-    height: 178,
+    height: 188,
     cornerRadius: 46,
     cornerSmoothing: 1
 });
@@ -19,7 +19,7 @@ const index = () => {
     return (
         <Layout>
             <main className='w-screen h-screen flex flex-col items-center justify-center'>
-                <div style={{ clipPath: `path('${svgPath}')` }} className='w-[479px] h-[400px] bg-white flex flex-col items-center px-4 py-4 justify-between'>
+                <div style={{ clipPath: `path('${svgPath}')` }} className='w-[479px] h-[410px] bg-white flex flex-col items-center px-4 py-4 justify-between'>
                     {/* <img src="/hero_img.svg" alt="" /> */}
                     <div className='w-full'>
                         <img className='h-full ' src="/hero.png" alt="" />
@@ -27,7 +27,10 @@ const index = () => {
                     </div>
 
                     <div className='flex flex-col items-center gap-1'>
-                        <p className="text-[#5E28BA] leading-3 font-baloo font-bold text-center">Succesfully Minted an NFT🎉<br />
+                        <p className="text-[#5E28BA] leading-3 font-baloo font-bold text-center">
+                            <span className="text-lg">
+                                azúcar!
+                            </span> <br />Succesfully Minted an NFT🎉<br />
                             <span className="text-sm text-[#AA7364]">(Check your wallet)</span>
                         </p>
 
